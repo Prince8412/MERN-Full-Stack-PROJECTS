aae25c28e5dd7c3ef9873762ff6dbbf74150d259
prince raj <100460531+Prince8412@users.noreply.github.com>
2024-02-26 16:07:12 +0530

    Add files via upload

---
+++ components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechyStar</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://www.linkedin.com/in/prince84/" target={"blank"}>
+            LinkedIN
+          </a>
+          <a href="https://www.instagram.com/princeraj84121/" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/Prince8412" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
